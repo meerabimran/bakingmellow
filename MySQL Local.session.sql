d01a55ea72dc0b56c021a008f3254083379f2bbb
meerabimran <themerbism@gmail.com>
2026-07-04 01:30:17 +0500

    First commit - Baking Mellow bakery website

---
+++ MySQL Local.session.sql
@@ -0,0 +1,20 @@
+INSERT INTO contact_messages (id, name, email, message)
+VALUES (
+    id:intINSERT INTO orders (id, user_id, total, order_date)
+    VALUES (
+        id:intINSERT INTO users (id, firstname, lastname, email, password)
+        VALUES (
+            id:int,
+            'firstname:varchar',
+            'lastname:varchar',
+            'email:varchar',
+            'password:varchar'
+          );,
+        user_id:int,
+        'total:decimal',
+        'order_date:timestamp'
+      );,
+    'name:varchar',
+    'email:varchar',
+    'message:text'
+  );
