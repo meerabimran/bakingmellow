0208cf8ddfb042940c482c8f351d19f272419f86
meerabimran <themerbism@gmail.com>
2026-07-19 19:45:02 +0500

    Updated db.php with live InfinityFree credentials

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'sql302.infinityfree.com';  // InfinityFree standard host
-$username = 'if0_XXXXXXX';           // The username you copied in Step 2
-$password = 'Your_Database_Password'; // The password you set in Step 2
-$database = 'if0_XXXXXXX_baking_mellow_db'; // The database name you copied in Step 2
+$host = 'sql113.infinityfree.com';  // InfinityFree standard host
+$username = 'if0_42432631';           // The username you copied in Step 2
+$password = 'Meerab11223344'; // The password you set in Step 2
+$database = 'if0_42432631_baking_mellow_db'; // The database name you copied in Step 2
 
 $conn = new mysqli($host, $username, $password, $database);
 
